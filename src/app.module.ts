7233e7c6963461d78d120290a1ebe74aac1ee6a3
Prashant <pkargathara7401@gmail.com>
2025-04-06 12:02:06 +0530

    Add Database URL in app Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { SmsService } from './sms/sms.service';
     ScheduleModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
+      url: process.env.DATABASE_URL,
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USER,
